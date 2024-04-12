5e3df978792f7d53ad343fd38fd2bfed52115c9c
Мария Плетнева <=>
2024-04-12 23:16:49 +0500

    added textfile model

---
+++ Models/TextFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientSide.Models
+{
+    internal class TextFile(string fileName, string text)
+    {
+        public string FileName { get; } = fileName;
+        public string Text { get; } = text;
+        public bool IsPalindrome { get; set; } = false;
+    }
+}
